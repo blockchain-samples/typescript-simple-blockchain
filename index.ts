46c414ce09ef9eb6ced7d3b4e6ad3fd6a44f2b15
jeo19 <terrakkk@outlook.com>
2020-05-15 23:44:47 +0800

    add:type in typescript

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 const name = 'Nicolas',
     age = '24',
     gender = 'male';
-const sayHi = (name, age, gender?) => {
-    console.log(`Hello ${name}, yau are age ${age}, you are a ${gender}`);
+const sayHi = (name: string, age: number, gender: string): string => {
+    return `Hello ${name}, yau are age ${age}, you are a ${gender}`;
 };
-sayHi(name, age);
+console.log(sayHi('Nicolas', 444, 'male'));
 export {};
