9c2b0bc723ebf6ad6eb47c7c7eaac9929a4482eb
jeo19 <terrakkk@outlook.com>
2020-05-16 00:15:06 +0800

    refactor:Add tsc-watch module, update package.json and tsconfig.json

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const name = 'Nicolas', age = '24', gender = 'male';
 const sayHi = (name, age, gender) => {
-    return `Hello ${name}, yau are age ${age}, you are a ${gender}`;
+    return `Hello ${name}, yau are age ${age}, you are a ${gender}!`;
 };
 console.log(sayHi('Nicolas', 444, 'male'));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ const name = 'Nicolas',
     age = '24',
     gender = 'male';
 const sayHi = (name: string, age: number, gender: string): string => {
-    return `Hello ${name}, yau are age ${age}, you are a ${gender}`;
+    return `Hello ${name}, yau are age ${age}, you are a ${gender}!`;
 };
 console.log(sayHi('Nicolas', 444, 'male'));
 export {};
