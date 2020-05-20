f9b00f300b664e53ffe1b6f55e4789059149d809
jeo19 <terrakkk@outlook.com>
2020-05-20 17:06:11 +0800

    feat:Add a 'validateStructure' method which to do validation for a block structure of blockchain and an 'isBlockValid' function which checks for validation of the current block and previous block

--- dist/index.js
+++ dist/index.js
@@ -11,7 +11,13 @@ let Block = /** @class */ (() => {
             this.timestamp = timestamp;
         }
     }
+    //Note:The method which declared by static is able to use without definning the Block class instance.
     Block.calculateBlockHash = (index, previousHash, data, timestamp) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+    Block.validateStructure = (aBlock) => typeof aBlock.index === 'number' &&
+        typeof aBlock.hash === 'string' &&
+        typeof aBlock.previousHash === 'string' &&
+        typeof aBlock.timestamp === 'number' &&
+        typeof aBlock.data === 'string';
     return Block;
 })();
 const genesisBlock = new Block(0, '2020202020202', '', 'Hello', 123456);
@@ -28,6 +34,17 @@ const createNewBlock = (data) => {
     const newHash = Block.calculateBlockHash(newIndex, previousBlock.hash, data, newTimeStamp);
     const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimeStamp);
     return newBlock;
+};
+const isBlockValid = (candidateBlock, previousBlock) => {
+    if (!Block.validateStructure(candidateBlock)) {
+        return false;
+    }
+    else if (previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    }
+    else if (previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    }
 };
 blockchain.push(createNewBlock('hello'));
 console.log(blockchain);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import * as CryptoJS from 'crypto-js';
 class Block {
-    public index: number;
-    public hash: string;
-    public previousHash: string;
-    public data: string;
-    public timestamp: number;
+    //Note:The method which declared by static is able to use without definning the Block class instance.
     static calculateBlockHash = (
         index: number,
         previousHash: string,
@@ -12,6 +8,17 @@ class Block {
         timestamp: number,
     ): string =>
         CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+    static validateStructure = (aBlock: Block): boolean =>
+        typeof aBlock.index === 'number' &&
+        typeof aBlock.hash === 'string' &&
+        typeof aBlock.previousHash === 'string' &&
+        typeof aBlock.timestamp === 'number' &&
+        typeof aBlock.data === 'string';
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
     constructor(
         index: number,
         hash: string,
@@ -51,6 +58,15 @@ const createNewBlock = (data: string): Block => {
         newTimeStamp,
     );
     return newBlock;
+};
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+    if (!Block.validateStructure(candidateBlock)) {
+        return false;
+    } else if (previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    } else if (previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    }
 };
 blockchain.push(createNewBlock('hello'));
 console.log(blockchain);
